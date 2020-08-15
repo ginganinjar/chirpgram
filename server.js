f851925c1a60d4e0ec99d3906076a6a36c0a90b9
David Sparrius <david@mstone.com.au>
2020-08-15 18:54:53 +1000

    socket io connection

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io').listen(server);
+
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -33,7 +34,7 @@ require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
@@ -42,33 +43,37 @@ db.sequelize.sync().then(() => {
   });
 });
 
-var numUsers = 0;
+let numUsers = 0;
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   var addedUser = false;
 
+  console.log("connection instigated");
+  
+
   // when the client emits 'new message', this listens and executes
-  socket.on('new message', (data) => {
+  socket.on("new message", (data) => {
+    console.log("a new message has been recieved");
     // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
+    socket.broadcast.emit("new message", {
       username: socket.username,
       message: data
     });
   });
 
   // when the client emits 'add user', this listens and executes
-  socket.on('add user', (username) => {
+  socket.on("add user", (username) => {
     if (addedUser) return;
 
     // we store the username in the socket session for this client
     socket.username = username;
     ++numUsers;
     addedUser = true;
-    socket.emit('login', {
+    socket.emit("login", {
       numUsers: numUsers
     });
     // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
+    socket.broadcast.emit("user joined", {
       username: socket.username,
       numUsers: numUsers
     });
@@ -76,25 +81,28 @@ io.on('connection', (socket) => {
 
   // when the client emits 'typing', we broadcast it to others
   socket.on('typing', () => {
+    console.log("the user is typing");
     socket.broadcast.emit('typing', {
       username: socket.username
     });
   });
 
   // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', () => {
-    socket.broadcast.emit('stop typing', {
+  socket.on("stop typing", () => {
+    console.log("the user has stopped typing");
+    socket.broadcast.emit("stop typing", {
       username: socket.username
     });
   });
 
   // when the user disconnects.. perform this
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
+    console.log("the user has disconnected");
     if (addedUser) {
       --numUsers;
 
       // echo globally that this client has left
-      socket.broadcast.emit('user left', {
+      socket.broadcast.emit("user left", {
         username: socket.username,
         numUsers: numUsers
       });

8f40eb73623cb6d57635ccbc658c070b9e3882b6
David Sparrius <david@mstone.com.au>
2020-08-22 11:43:35 +1000

    corrected route issue

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,17 +7,8 @@ module.exports = function(app) {
     if (req.user) {
       res.render("chat");
     }
-    res.render("login");
-  });
-
-  app.get("/signup", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.render("chat");
-    }
     res.render("signup");
   });
-
 
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
@@ -26,14 +17,28 @@ module.exports = function(app) {
     }
     res.render("login");
   });
-
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
+    if (!req) {
+      return res.status(400).json({
+        status: 'error',
+        error: 'req body cannot be empty',
+      });
+    }
+
     res.render("chat");
   });
 
   app.get("/profile", (req, res) => {
+
+    if (!req) {
+      return res.status(400).json({
+        status: 'error',
+        error: 'req body cannot be empty',
+      });
+    }
+
     res.render("profile");
   });
 };

0b58e34f769b3d2cc4e673216bb3e8f09c6b1325
Ben Stephens <ben_j_stephens@hotmail.com>
2020-08-22 12:36:27 +1000

    now populates the text box with db info

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,11 +2,32 @@ $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/profile").then(data => {
-    $("#avatar").text(data.avatar);
-    $("#bio").text(data.bio);
-    $("#location").text(data.location);
-    $("#likes").text(data.likes);
-    $("#email").text(data.email);
-    $("#phone").text(data.phone);
+    $("#avatar").attr("src", data.avatar);
+    $("#bio").val(data.bio);
+    $("#location").val(data.location);
+    $("#likes").val(data.likes);
+    $("#email").val(data.email);
+    $("#phone").val(data.phone);
+  });
+
+  $("#uploadForm").submit(function() {
+    $("#status")
+      .empty()
+      .text("File is uploading...");
+
+    $(this).ajaxSubmit({
+      error: function(xhr) {
+        status("Error: " + xhr.status);
+      },
+
+      success: function(response) {
+        console.log(response);
+        $("#status")
+          .empty()
+          .text(response);
+      }
+    });
+
+    return false;
   });
 });
